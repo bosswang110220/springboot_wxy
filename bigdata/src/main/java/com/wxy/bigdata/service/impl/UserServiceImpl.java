7be62a44d55d50264f306395f53b9f44635d3962
bosswang110220 <18787141931@163.com>
2019-07-03 16:39:36 +0800

    配置文件生成

---
+++ bigdata/src/main/java/com/wxy/bigdata/service/impl/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.wxy.bigdata.service.impl;
+
+import com.wxy.bigdata.entity.User;
+import com.wxy.bigdata.mapper.UserMapper;
+import com.wxy.bigdata.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+@Autowired
+   private  UserMapper userMapper;
+
+    @Override
+    public int deleteByPrimaryKey(Long id) {
+        return userMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public int insert(User record) {
+        return userMapper.insert(record);
+    }
+
+    @Override
+    public User selectByPrimaryKey(Long id) {
+        return userMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public List<User> selectAll() {
+        return userMapper.selectAll();
+    }
+
+    @Override
+    public int updateByPrimaryKey(User record) {
+        return userMapper.updateByPrimaryKey(record);
+    }
+}
